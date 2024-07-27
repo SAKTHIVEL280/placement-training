20d2d1f9eea5090c318d799da9d9730667910ef5
SAKTHIVEL_E <148361514+SAKTHIVEL280@users.noreply.github.com>
2024-07-27 18:46:35 +0530

    Update find the index of first occurrence in a string.py

--- 27.07.2024/find the index of first occurrence in a string.py
+++ 27.07.2024/find the index of first occurrence in a string.py
@@ -5,11 +5,5 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        st=haystack
-        s=needle
-        for i in range(len(st)):
-            if s[0]==st[i]:
-                if s==st[i:len(s)]:
-                    return i
-            else:
-                return -1
+        ind=haystack.find(needle)
+        return ind
