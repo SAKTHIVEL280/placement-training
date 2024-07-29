85b2a4aa43a8175550e1cc65a071ce8eca9329c9
SAKTHIVEL_E <148361514+SAKTHIVEL280@users.noreply.github.com>
2024-07-29 19:04:19 +0530

    Create remove element.py

---
+++ 29.07.2024/remove element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        for i in nums[::-1]:
+            if i == val:
+                nums.remove(val)
